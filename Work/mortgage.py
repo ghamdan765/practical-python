dc61849ab37bf910e8efcd26aa452bb2064ef403
Ghamdan Al-Naggar <ghamdan765@gmail.com>
2021-07-26 20:02:59 +0300

    Ex. From 1.7 To 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,62 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+# %%
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print('Total paid', total_paid)
+# %%
+# Exercise 1.8
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+mounth = 0
+while principal > 0:
+    mounth += 1
+    if mounth <= 12:
+        principal = principal * (1+rate/12) - (payment+1000)
+        total_paid = total_paid + (payment+1000)
+    else:
+        payment = 2684.11
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+print('Total paid', total_paid, 'mounths', mounth)
+
+# %%
+# Exercise 1.9
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    print(month, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
+print('Months', month)
+# %%
